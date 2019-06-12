8546e2ce0e3fa0338a0bfa78476c8d67b03352fa
hsvarma2092 <varmah2092@gmail.com>
2019-06-11 21:29:16 -0700

    new

--- bash/network-config.sh
+++ bash/network-config.sh
@@ -36,13 +36,27 @@
 MyHost=$(hostname)
 interface=$(ip a |awk '/: e/{gsub(/:/,"");print $2}')
 LAN_Address=$(ip a s $interface |awk '/inet /{gsub(/\/.*/,"");print $2}')
-LAN_Hostname=$(getent hosts $LAN_Address|awk '/inet /{gsub(/\/.*/,"");print $2}' | awk '{print $2}')
+LAN_Hostname=$(getent hosts $LAN_Address| awk '{print $2}')
 External_IP=$(curl -s icanhazip.com)
-External_Name=$(getent hosts $External_IP | awk '{print $2}')
+Router_address=$(ip route | grep 'default' | awk '{ print $3}')
+Router_name=$(getent hosts $Router_address| awk '{print $2}')
+Network_Address=$(route -n | awk '/255.255.255.0/''{print $1}')
+Network_Name=$(getent networks $Network_Address| awk '{print $1}')
 cat <<EOF
 Hostname      : $MyHost
+
 LAN IP        : $LAN_Address
+
 LAN Hostname  : $LAN_Hostname
+
 External IP   : $External_IP
-External name : $External_Name
+
+Router address: $Router_address
+
+Router Name   : $Router_name
+
+Network IP    : $Network_Address
+
+Network NAME  : $Network_Name
+
 EOF
